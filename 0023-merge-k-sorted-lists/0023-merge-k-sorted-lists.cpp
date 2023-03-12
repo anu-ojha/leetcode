2c179880ea2fb08bae364383aefd5c0f44849b02
Anurag Ojha <97898685+anu-ojha@users.noreply.github.com>
2023-03-12 09:51:33 +0530

    Time: 915 ms (5.01%), Space: 13.6 MB (34.63%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,44 @@
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n=lists.size();
+        if(n==0){
+            return NULL;
+        }
+        ListNode *l=new ListNode();
+        ListNode *head=l;
+        ListNode *prev;
+        int _empty=1;
+        while(1){
+            int val=INT_MAX;
+            int index=0;
+            int possible=0;
+            for(int i=0;i<n;i++){
+                if(lists[i]!=NULL&&lists[i]->val<val){
+                    val=lists[i]->val;
+                    index=i;
+                }
+            }
+            // now we have minimum in current
+            if(val==INT_MAX){
+                break;
+            }else{
+                // create new node
+                _empty=0;
+                l->val=val;
+                prev=l;
+                l->next=new ListNode();
+                l=l->next;
+                lists[index]=lists[index]->next;
+            }
+        }
+        if(head->next!=NULL){
+            prev->next=NULL;
+        }
+        if(_empty){
+            head=NULL;
+        }
+        return head;
+    }
+};
